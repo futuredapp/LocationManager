9c1ad87598e4d69c7d36656ce3d631073ef2e6d6
TFTmilan <milan@Milans-MacBook-Air.local>
2016-09-20 12:40:25 +0200

    Remove pods directory

--- Example/Pods/Target Support Files/LocationManager/LocationManager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LocationManager : NSObject
-@end
-@implementation PodsDummy_LocationManager
-@end
--- Example/Pods/Target Support Files/Pods-LocationManager_Example/Pods-LocationManager_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_LocationManager_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_LocationManager_ExampleVersionString[];
-
